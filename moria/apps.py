c6303f2dc9d9868ae73dda76644e17805d0ea78a
14cky <113927878+14cky@users.noreply.github.com>
2024-08-18 11:18:56 +0500

    Delete moria directory

--- moria/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MoriaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'moria'
-
-    def ready(self):
-        import moria.signals
